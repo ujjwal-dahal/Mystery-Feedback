83ef6bb200766c984df38f9bfbd7cd6bd482b743
ujjwal-dahal <toppelp@gmail.com>
2024-11-20 21:04:26 +0545

    User Model is created

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,8 +1,35 @@
 import mongoose , {Schema , Document} from "mongoose";
+import { Message } from "./Message.model";
 
+
+
+export interface User extends Document {
+  username : string,
+  email : string ,
+  password : string,
+  verifyCode : string ,
+  verifyCodeExpiry : Date,
+  isAcceptingMessage : boolean ,
+  message : Message[]
+}
 
 
 
 const UserSchema = new Schema({
+  username : {
+    type : String,
+    required : true
+  },
+  email :{
+    type : String,
+    required : true
+  },
+  password : {
+    type : String,
+    required : true
+  },
+  verifyCode : String,
+  verifyCodeExpiry : Date,
+  isAcceptingMessage : Boolean
   
 })
