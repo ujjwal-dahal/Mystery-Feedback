47c88369df6b729944954d230bd8aa524754ede1
ujjwal-dahal <toppelp@gmail.com>
2024-11-22 11:57:10 +0545

    Schemas are created using ZOD

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,7 @@
+import {z} from "zod";
+
+export const messageSchema = z.object({
+  content : z.string()
+  .min(10 , "Content must contain at least 10 characters")
+  .max(300 , "Content must not be longer than 300 characters")
+})
