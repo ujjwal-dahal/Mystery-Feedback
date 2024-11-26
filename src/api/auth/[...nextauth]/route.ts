3d24e4d83c538af30e89b3496a83227b2bde29b9
ujjwal-dahal <toppelp@gmail.com>
2024-11-27 00:59:16 +0545

    Auth is checked using signin page

--- src/api/auth/[...nextauth]/route.ts
+++ src/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "./options";
 
 
-const handler = NextAuth(authOptions)
+const handlers = NextAuth(authOptions)
+
+export {handlers as GET , handlers as POST};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { SessionProvider } from "next-auth/react";
+import { AuthProvider } from "@/context/AuthProvider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +17,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <AuthProvider>
+      <body>
         {children}
       </body>
+      </AuthProvider>
     </html>
   );
 }
