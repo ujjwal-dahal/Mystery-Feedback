810b807dba559825cf5f7126ffe469a72781ab63
ujjwal-dahal <toppelp@gmail.com>
2024-11-29 22:49:20 +0545

    Accept Message Route is Created

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -63,8 +63,8 @@ export const authOptions = {
     }),
 
     Google({
-      clientId : process.env.GOOGLE_CLIENT_ID,
-      clientSecret : process.env.GOOGLE_CLIENT_SECRET
+      clientId : process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret : process.env.GOOGLE_CLIENT_SECRET as string
     }),   
   ]
 
