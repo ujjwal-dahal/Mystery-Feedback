ab7b5ee92f293d85aae450894cf57f56223922d2
ujjwal-dahal <toppelp@gmail.com>
2024-12-03 23:54:25 +0545

    Navbar is created

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,42 @@
 "use client";
 import Link from "next/link";
-import { useSession , signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { User } from "next-auth";
+import "./Navbar.scss";
 
+const Navbar = () => {
+  const { data: session } = useSession();
 
-const Navbar = ()=>{
-  const {data} = useSession();
-  
+  const user: User = session?.user as User;
 
-return <>
-
-</>
-
-}
-
+  return (
+    <>
+      <div className="navbar">
+        <div className="title">
+          <Link href={"/"}>Mystery Feedback</Link>
+        </div>
+        <div className="link">
+          <Link href={"/about"}>About</Link>
+          <Link href={"/contact"}>Contact</Link>
+          <Link href={"/disclaimer"}>Disclaimer</Link>
+        </div>
+        <div className="button-area">
+          {session ? (
+            <button onClick={() => signOut()}>Signout</button>
+          ) : (
+            <>
+              <Link href={"/sign-up"}>
+                <button>Signup</button>
+              </Link>
+              <Link href={"/sign-in"}>
+                <button>Signin</button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
