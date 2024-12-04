331428463c9eb6524a03f49dcb7052cbabcc78f9
ujjwal-dahal <toppelp@gmail.com>
2024-12-05 03:20:57 +0545

    Color combination Corrected and Error has been fixed

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { Mail } from "lucide-react"; // Assuming you have an icon for messages
+import { Mail } from "lucide-react";
 import { useEffect } from "react";
 import Autoplay from "embla-carousel-autoplay";
 import messages from "@/messages.json";
@@ -15,18 +15,17 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Card, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <>
-      {/* Main content */}
       <main className="home-main">
         <section className="home-header">
           <h1>Explore the Power of Anonymous Feedback</h1>
           <p>Your insights, without revealing your identity.</p>
         </section>
 
-        {/* Carousel for Messages */}
         <Carousel
           plugins={[Autoplay({ delay: 2000 })]}
           className="message-carousel"
@@ -34,18 +33,18 @@ export default function Home() {
           <CarouselContent>
             {messages.map((message, index) => (
               <CarouselItem key={index} className="carousel-item">
-                <div className="message-card">
-                  <div className="card-header">
-                    <h2>{message.title}</h2>
-                  </div>
+                <Card className="message-card">
+                  <CardHeader className="card-header">
+                  <h2>{message.title}</h2>
+                  </CardHeader>
                   <div className="card-content">
                     <Mail className="mail-icon" />
                     <div className="card-text">
                       <p>{message.content}</p>
                       <p className="card-received">{message.received}</p>
-                    </div>
                   </div>
                 </div>
+                </Card>
               </CarouselItem>
             ))}
           </CarouselContent>
